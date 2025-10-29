d63e02c87b45198422daae76aa3fb1bad15bf964
kn2117 <89276194+kn2117@users.noreply.github.com>
2025-10-29 14:43:33 -0700

    Implemented most buttons for header
    Still need to make the menus expand, and otherwise space things out

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,55 @@
+import { Button, Toolbar } from '@mui/material';
 import './Header.css'
+import TextField from '@mui/material/TextField';
+import logo from './assets/logo.png';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Header() {
     return (
         <>
             <div class="fullHeaderContainer">
                 <div class="leftSideHeaderContainer fullHeaderItem">
-                    <p>This is the logo</p>
+                    <img class="logo leftSideHeaderItem" src={logo} />
                     <div class="headerStackContainer leftSideHeaderItem">
-                        <p>This is the file name</p>
-                        <p>This is the file menu thing</p>
+                        <TextField class="titleTextBox" id="outlined-basic" label="" variant="outlined" size="small" defaultValue="My Document" />
+                        <div class="menuBarContainer leftSideHeaderItem">
+                            <Toolbar variant="dense" disableGutters={true}>
+                                <Button id="basic-button" color="black">
+                                    File
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    Edit
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    View
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    Insert
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    Format
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    Tools
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    Extensions
+                                </Button>
+                                <Button id="basic-button" color="black">
+                                    Help
+                                </Button>
+                            </Toolbar>
+                        </div>
                     </div>
                 </div>
                 <div class="rightSideHeaderContainer fullHeaderItem">
                     <div class="rightSideHeaderItem">
-                        <p>This is the share button</p>
+                        <Button variant='contained'>
+                            Share
+                        </Button>
                     </div>
                     <div class="rightSideHeaderItem">
-                        <p>This is the profile icon</p>
+                        <AccountCircleIcon fontSize='large'/>
                     </div>
                 </div>
                 
