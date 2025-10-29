0144b8b9e87832dbaf8bde8afb03cfa2b30472b3
kn2117 <89276194+kn2117@users.noreply.github.com>
2025-10-29 10:40:28 -0700

    Skeleton for header section
    Includes a new CSS file due to the use of the flexbox layout

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,26 @@
+import './Header.css'
+
 export default function Header() {
-    return <header>This is the header section</header>
+    return (
+        <>
+            <div class="fullHeaderContainer">
+                <div class="leftSideHeaderContainer fullHeaderItem">
+                    <p>This is the logo</p>
+                    <div class="headerStackContainer leftSideHeaderItem">
+                        <p>This is the file name</p>
+                        <p>This is the file menu thing</p>
+                    </div>
+                </div>
+                <div class="rightSideHeaderContainer fullHeaderItem">
+                    <div class="rightSideHeaderItem">
+                        <p>This is the share button</p>
+                    </div>
+                    <div class="rightSideHeaderItem">
+                        <p>This is the profile icon</p>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
 }
