342948cd72b7454d3a2d7e1b885bec180f40ba73
kn2117 <89276194+kn2117@users.noreply.github.com>
2025-10-29 19:05:45 -0700

    Added a function for the menu items

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,63 +1,104 @@
-import { Button, Menu, Toolbar } from '@mui/material';
-import './Header.css'
-import TextField from '@mui/material/TextField';
+import * as React from 'react';
+import './Header.css';
 import logo from './assets/logo.png';
+import { Button, IconButton, Menu, MenuItem, Toolbar, TextField } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HistoryIcon from '@mui/icons-material/History';
+import CommentIcon from '@mui/icons-material/Comment';
+import PublicIcon from '@mui/icons-material/Public';
 
 export default function Header() {
     return (
-        <>
-            <div class="fullHeaderContainer">
-                <div class="leftSideHeaderContainer fullHeaderItem">
-                    <img class="logo leftSideHeaderItem" src={logo} />
-                    <div class="headerStackContainer leftSideHeaderItem">
-                        <TextField class="titleTextBox headerStackItem" id="outlined-basic" label="" variant="outlined" size="small" defaultValue="My Document" />
-                        <div class="menuBarContainer headerStackItem">
-                            <Toolbar variant="dense" disableGutters={true}>
-                                <MenuItemButton name="File" />
-                                <MenuItemButton name="Edit" />
-                                <MenuItemButton name="View" />
-                                <MenuItemButton name="Insert" />
-                                <MenuItemButton name="Format" />
-                                <MenuItemButton name="Tools" />
-                                <MenuItemButton name="Extensions" />
-                                <MenuItemButton name="Help" />
-                            </Toolbar>
-                        </div>
+        <div class="fullHeaderContainer">
+            <div class="leftSideHeaderContainer fullHeaderItem">
+                <img class="logo leftSideHeaderItem" src={logo} />
+                <div class="headerStackContainer leftSideHeaderItem">
+                    <TextField class="titleTextBox headerStackItem" id="outlined-basic" label="" variant="outlined" size="small" defaultValue="My Document" />
+                    <div class="menuBarContainer headerStackItem">
+                        <Toolbar variant="dense" disableGutters={true}>
+                            <MenuItemButton name="File" />
+                            <MenuItemButton name="Edit" />
+                            <MenuItemButton name="View" />
+                            <MenuItemButton name="Insert" />
+                            <MenuItemButton name="Format" />
+                            <MenuItemButton name="Tools" />
+                            <MenuItemButton name="Extensions" />
+                            <MenuItemButton name="Help" />
+                        </Toolbar>
                     </div>
                 </div>
-                <div class="rightSideHeaderContainer fullHeaderItem">
-                    <div class="rightSideHeaderItem">
-                        <Button>
-                            History
-                        </Button>
-                        <Button>
-                            Comments
-                        </Button>
-                    </div>
-                    <div class="rightSideHeaderItem">
-                        <Button variant='contained'>
-                            Share
-                        </Button>
-                    </div>
-                    <div class="rightSideHeaderItem">
-                        <Button>
-                            <AccountCircleIcon fontSize='large'/>
-                        </Button>
-                    </div>
-                </div>
-                
             </div>
-        </>
-    )
+            <div class="rightSideHeaderContainer fullHeaderItem">
+                <div class="rightSideHeaderItem">
+                    <IconButton>
+                        <HistoryIcon />
+                    </IconButton>
+                </div>
+                <div class="rightSideHeaderItem">
+                    <IconButton>
+                        <CommentIcon />
+                    </IconButton>
+                </div>
+                <div class="rightSideHeaderItem">
+                    <Button variant='contained' startIcon={<PublicIcon />}>
+                        Share
+                    </Button>
+                </div>
+                <div class="rightSideHeaderItem">
+                    <IconButton>
+                        <AccountCircleIcon fontSize='large'/>
+                    </IconButton>
+                </div>
+            </div>
+            
+        </div>
+    );
 }
 
 function MenuItemButton({name}) {
     return (
-        <>
-            <Button id="basic-button" color="black">
-                {name}
-            </Button>
-        </>
-    )
+        <Button color="black" size="small">
+            {name}
+        </Button>
+    );
+}
+
+function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            'aria-labelledby': 'basic-button',
+          },
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
 }
