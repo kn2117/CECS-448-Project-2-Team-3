08f6ab0f73e0bb01bcfbdb569feaa517a1e1335f
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-29 21:24:12 -0700

    Added a document body with text similar to google doc styling.

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,30 @@ import DocumentTabs from './DocumentTabs.jsx'
 import EditingModes from './EditingModes.jsx'
 import RibbonMenu from './RibbonMenu.jsx'
 import MainDocWindow from './MainDocWindow.jsx'
+import DocBody from './DocBody.jsx'
 
 function App() {
   return (
     <>
       <div class="header">
         <Header />
-      </div>
-      <div class="documentTabs">
-        <DocumentTabs />
-      </div>
-      <div class="editingModes">
-        <EditingModes />
-      </div>
-      <div class="ribbonMenu">
-        <RibbonMenu />
-      </div>
-      <div class="mainDocWindow">
-        <MainDocWindow />
-      </div>
-    </>
+       </div>
+       <DocBody/>
+      
+       {/* <div class="documentTabs">
+         <DocumentTabs />
+       </div>
+       <div class="editingModes">
+         <EditingModes />
+       </div>
+       <div class="ribbonMenu">
+         <RibbonMenu />
+       </div>
+       <div class="mainDocWindow">
+         <MainDocWindow />
+       </div> */}
+     </>
+  
   )
 }
 
