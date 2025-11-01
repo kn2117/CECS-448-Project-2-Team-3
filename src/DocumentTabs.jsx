a85300877565b66563d81afc0f7b2a0b6346ef9d
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-31 17:54:45 -0700

    Added document tabs with unique text to display

--- src/DocumentTabs.jsx
+++ src/DocumentTabs.jsx
@@ -1,3 +1,42 @@
-export default function DocumentTabs() {
-    return <header>This is the document tabs section</header>
+import { Button } from "@mui/material"
+import "./DocumentTabs.css"
+import { BorderLeft, Margin } from "@mui/icons-material";
+
+const styles={
+    tab:{
+        display: 'inline-block',
+        paddingLeft: 10, paddingRight: 10,borderTop:'2px solid #c8c8c8', borderLeft:'2px solid #c8c8c8', borderRight:'2px solid #c8c8c8', borderTopLeftRadius:5, borderTopRightRadius:5,
+    },
+    button:{
+        borderTop:'2px solid #c8c8c8', borderLeft:'2px solid #c8c8c8', 
+        borderRight:'2px solid #c8c8c8', borderTopLeftRadius:5, borderTopRightRadius:5, 
+        borderBottomLeftRadius:0, borderBottomRightRadius:0, outline:'none', color:'black',
+    }
+};
+
+function Tab({text, id, value, onClick}) {
+    return(
+        <div style={{... styles.tab, backgroundColor:(value == id)?'white':'#e4e4e4ff', textAlign:'center', height:(value == id)?25:undefined}}
+            onClick={()=>onClick(id)}>
+            {text}
+        </div>
+    );
+};
+
+
+export default function DocumentTabs({value, onClick}) {
+    return (
+        <>
+            <div class='TabContainer'
+            >
+               <Tab text="tab 1" id={1} value={value} onClick={onClick}/>
+               <Tab text="tab 2" id={2} value={value} onClick={onClick}/>
+               <Tab text="tab 3" id={4} value={value} onClick={onClick}/>
+               <Button style={{...styles.button}}>
+                +
+               </Button>
+            </div>
+        
+        </>
+    )
 }
