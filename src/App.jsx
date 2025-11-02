1c02f232ed54b920c701acf14d808b73f00edb47
ArthurO-CSULB <arthurking.ordonez01@student.csulb.edu>
2025-11-01 23:57:46 -0700

    Added buttons for mode changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,33 @@ import EditingModes from './EditingModes.jsx'
 import RibbonMenu from './RibbonMenu.jsx'
 import MainDocWindow from './MainDocWindow.jsx'
 import DocBody from './DocBody.jsx'
+import * as React from 'react';
 
 function App() {
+
+  // state variable for checking which mode to use
+  const [mode, setMode] = React.useState("view")
+
+  // helper function for mode changes
+  const handleModeChange = (newMode) => {
+    setMode(newMode);
+  };
+
   return (
     <>
       <div class="header">
         <Header />
+        <EditingModes
+          currentMode = {mode}
+          handleModeChange = {handleModeChange}/>  
       </div>
       <div class="docBody">
         <DocBody/>
       </div>
-      
        {/* <div class="documentTabs">
          <DocumentTabs />
        </div>
-       <div class="editingModes">
-         <EditingModes />
-       </div>
+
        <div class="ribbonMenu">
          <RibbonMenu />
        </div>
